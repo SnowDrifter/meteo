41871b945dd280cd834595ed0b7027a7a4620792
Ivan <fl125@yandex.ru>
2018-10-16 02:26:51 +0300

    Add BME280 support via SPI

--- meteo.ino
+++ meteo.ino
@@ -1,4 +1,5 @@
 #include <Arduino.h>
+#include <Adafruit_BMP280.h>
 #include <math.h>
 #include <U8g2lib.h>
 #include <SPI.h>
@@ -6,9 +7,14 @@
 #include "cactus_io_AM2302.h"
 
 #define AM2302_PIN 2
+#define BMP_SCK 13
+#define BMP_MISO 10
+#define BMP_MOSI 12
+#define BMP_CS 11
 
 U8G2_SSD1306_128X32_UNIVISION_F_HW_I2C u8g2(U8G2_R0);
 AM2302 dht(AM2302_PIN);
+Adafruit_BMP280 bme(BMP_CS, BMP_MOSI, BMP_MISO,  BMP_SCK);
 
 void setup() {
   Serial.begin(9600);
@@ -20,6 +26,8 @@ void loop() {
   updateHumidity();
   delay(2000);
   updateTemperature();
+  delay(2000);
+  updatePressure();
   delay(2000);
 }
 
@@ -50,9 +58,25 @@ void updateTemperature() {
   float temperature = dht.temperature_C;
 
   char result[8];
-  char str_temperature[6];
+  char str_temperature[5];
   dtostrf(temperature, 3, 1, str_temperature);
   sprintf(result, "%s*C", str_temperature);
+
+  printToMonitor(result);
+}
+
+void updatePressure() {
+  if (!bme.begin()) {
+    Serial.println("BME280 sensor read failure!");
+    return;
+  }
+
+  float pressure = bme.readPressure() / 133.3; // Convert from Pa to mmHg
+
+  char result[8];
+  char str_pressure[6];
+  dtostrf(pressure, 3, 1, str_pressure);
+  sprintf(result, "%smm", str_pressure);
 
   printToMonitor(result);
 }
